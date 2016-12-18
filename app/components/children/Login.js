cd3b3ae561e38b0434b3ba23e2569702332b688d
Andrea R <amrdev08@gmail.com>
2016-12-18 00:02:36 -0500

    skeleton

---
+++ app/components/children/Login.js
@@ -0,0 +1,42 @@
+var React = require("react");
+
+
+  class Login extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {value: ''};
+
+      this.handleChange = this.handleChange.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+      this.setState({value: event.target.value});
+    }
+
+    handleSubmit(event) {
+      alert('A name was submitted: ' + this.state.value);
+      event.preventDefault();
+    }
+
+    render() {
+      return (
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            User Name:
+            <input type="text" className="validate" value={this.state.value} onChange={this.handleChange} />
+          </label>
+          <label>
+            Password
+            <input type="text" className="validate" value={this.state.value} onChange={this.handleChange} />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+      );
+    }
+  }
+
+  
+
+
+module.exports = Login;
