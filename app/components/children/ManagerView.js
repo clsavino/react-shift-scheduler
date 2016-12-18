894f54ab1b10cbe3a55515fbbbe5cefc0aa69aa9
Nicolas Caceres <nicolas.caceres89@gmail.com>
2016-12-18 17:35:22 -0500

    manager and employees dashboard setup

---
+++ app/components/children/ManagerView.js
@@ -0,0 +1,15 @@
+var React = require("react");
+
+var ManagerView = React.createClass({
+    render: function() {
+        return (
+            <div className="row">
+                <div className="col m12">
+                    <h1> ManagerView goes here</h1>
+                </div>
+            </div>
+        );
+    }
+});
+
+module.exports = ManagerView;
