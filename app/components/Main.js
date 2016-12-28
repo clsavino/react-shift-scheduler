abe1ca592a756856fc7a75293639c88dbc7f7da4
Nicolas Caceres <nicolas.caceres89@gmail.com>
2016-12-28 17:04:54 -0500

    new employee mgmt idea

--- app/components/Main.js
+++ app/components/Main.js
@@ -5,28 +5,7 @@ var Main = React.createClass({
     render: function() {
         return (
             <div>
-              <nav>
-                    <div className="nav-wrapper grey darken-3">
-    
-                      <a href="#/login" className="brand-logo center blue-text text-darken-1"><img id="logo" src="assets/images/logo.png"/>Schedulr</a>
-
-                      <a href="#/login" data-activates="mobile-demo" className="button-collapse"><i className="material-icons blue-text text-darken-1">menu</i></a>
-                      <ul className="right hide-on-med-and-down">
-                        <li><a href="#/login">Login</a></li>
-                        <li><a href="#/register">Register</a></li>
-                        <li><a href="/logout">Logout</a></li>
-                      </ul>
-                      <ul className="side-nav" id="mobile-demo">
-                        <li><a href="#/login">Login</a></li>
-                        <li><a href="#/register">Register</a></li>
-                        <li><a href="/logout">Logout</a></li>
-                      </ul>
-                    </div>
-                  </nav>
-
-                <div className="container">
-                    {this.props.children}
-                </div>
+                {this.props.children}
             </div>
         );
     }
--- app/components/children/Login.js
+++ app/components/children/Login.js
@@ -22,32 +22,56 @@ class Login extends React.Component {
   }
     render() {
       return (
-        <div className="container" id="loginForm">
-          <form action="/login" method="POST" onSubmit={this.handleLogin}>
-            <h4 id="spacing">Login</h4>
-            <label for="username"> User Name: </label>
-              <input type="text" 
-                className="validate" 
-                value={this.state.username} 
-                name="username" 
-                onChange={this.handleUserChange}
-                required />
-
-            <label for="password">Password: </label>
-              <input type="password" 
-                className="validate" 
-                value={this.state.password} 
-                name="password" 
-                onChange={this.handleUserChange}
-                required  />
-
-            <button className="btn waves-effect waves-light btn-large blue accent-3" type="submit" value="Submit" name="action">Submit
-            <i className="material-icons right">send</i>
-            </button>
-          </form>
+        <div className="container">
+            <div className="row" id="loginForm">
+                <div className="col m8 offset-m2">
+                    <div className="card-panel">
+                        <div className="row grey lighten-5">
+                            <div className="col s12 center">
+                                <h4 className="blue-text text-darken-1"><img id="logo" src="assets/images/logo.png"/><span className="hide-on-med-and-down">Schedulr</span></h4>
+                            </div>
+                        </div>
+                        <form action="/login" method="POST" onSubmit={this.handleLogin}>
+                            <div className="row">
+                                <div className="col s12">
+                                    <input
+                                        placeholder="Username"
+                                        type="text"
+                                        className="validate"
+                                        value={this.state.username}
+                                        name="username"
+                                        onChange={this.handleUserChange} />
+                                </div>
+                            </div>
+                            <div className="row">
+                                <div className="col s12">
+                                    <input
+                                        placeholder="Password"
+                                        type="password"
+                                        className="validate"
+                                        value={this.state.password}
+                                        name="password"
+                                        onChange={this.handleUserChange} />
+                                </div>
+                            </div>
+                            <div className="row">
+                                <div className="col m4 s12 left">
+                                    <button className="btn waves-effect waves-light btn-large blue accent-3 loginButtons left" type="submit" value="Submit" name="action">Login<i className="material-icons right">send</i></button>
+                                </div>
+                                <div className="col m4 s12 center">
+                                    <a className="btn waves-effect waves-light btn-large green accent-3 loginButtons center" href="#/register">Register<i className="material-icons right">person_add</i></a>
+                                </div>
+                                <div className="col m4 s12 right">
+                                    <a className="btn waves-effect waves-light btn-large red accent-3 loginButtons right" href="#/logout">Logut<i className="material-icons right">exit_to_app</i></a>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
         </div>
       );
     }
-  };
+};
 
 module.exports = Login;
