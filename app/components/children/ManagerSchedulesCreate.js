3de92ff6bd90b6763d8461e0cb66df418115b4d7
Nicolas Caceres <nicolas.caceres89@gmail.com>
2017-01-01 16:03:41 -0500

    manager section cleanup

--- app/components/children/ManagerSchedulesCreate.js
+++ app/components/children/ManagerSchedulesCreate.js
@@ -1,15 +1,15 @@
 var React = require("react");
 
-var ManagerSchedulesView = React.createClass({
+var ManagerSchedulesCreate = React.createClass({
     render: function() {
         return (
             <div className="row">
                 <div className="col m12">
-                    <h1>View upcoming schedules</h1>
+                    <h1>Create schedules</h1>
                 </div>
             </div>
         );
     }
 });
 
-module.exports = ManagerSchedulesView;
+module.exports = ManagerSchedulesCreate;
