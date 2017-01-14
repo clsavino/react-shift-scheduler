6f74b3d4290e877a0a2611ae0fdf4b52053fa54a
Nicolas Caceres <nicolas.caceres89@gmail.com>
2017-01-14 14:49:58 -0500

    updated styling

--- app/components/Employee.js
+++ app/components/Employee.js
@@ -7,7 +7,7 @@ var Employee = React.createClass({
             <div>
                 <nav>
                     <div className="nav-wrapper grey lighten-5">
-                        <a className="brand-logo blue-text text-darken-1"><img id="logo" src="/assets/images/logo.png"/><span className="hide-on-med-and-down">Hello [Employee]</span></a>
+                        <a href="/employee" className="brand-logo blue-text text-darken-1"><img id="logo" src="/assets/images/logo.png"/><span className="hide-on-med-and-down">Schedulr</span></a>
                         <a href="/" data-activates="slide-out" className="button-collapse blue-text text-darken-1"><i className="material-icons">menu</i></a>
                         <ul className="right hide-on-med-and-down">
                             <li><a className="black-text" href="/employee">Home<i className="material-icons right">home</i></a></li>
