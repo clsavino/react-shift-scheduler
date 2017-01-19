703d7e08f6250788a698c28832c62bddffa1f2c5
Houston Breedlove <houston@guest-2-00751.dhcp.unc.edu>
2017-01-19 17:49:22 -0500

    successfully showing announcements on employee's view page

--- app/components/children/AnnouncementsView.js
+++ app/components/children/AnnouncementsView.js
@@ -2,28 +2,39 @@ var React = require("react");
 var helpers = require("../utils/helpers");
 
 var AnnouncementsView = React.createClass({
-    // getInitialState: function() {
-    //     return {
-    //         title: "",
-    //         content: "",
-    //         datetime: ""
-    //     };
-    // },
-    //
-    // componentDidMount: function() {
-    //     this.getAnnouncements();
-    // },
-    //
-    // getAnnouncements: function() {
-    //     helpers.getAnnouncements().then(function(response) {
-    //
-    //     }.bind(this));
-    // },
+    getInitialState: function() {
+        return {
+            title: "",
+            content: "",
+            datetime: ""
+        };
+    },
+
+    componentDidMount: function() {
+        this.getAnnouncements();
+    },
+
+    getAnnouncements: function() {
+        helpers.getAnnouncements().then(function(response) {
+          console.log(response.data[response.data.length - 1]);
+          this.setState({
+            title: response.data[response.data.length -1].title,
+            content: response.data[response.data.length -1].content
+          });
+
+
+          if (response !== this.state.allEmployees) {
+              this.setState({ allEmployees: response.data });
+              this.activeButtons();
+          }
+        }.bind(this));
+    },
 
     render: function() {
         return (
             <div>
-                <p>View announcements here</p>
+                <h5>{this.state.title}</h5>
+                <p>{this.state.content}</p>
             </div>
         );
     }
